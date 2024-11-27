e1a0e2ad023d81918625cbb47824a4fc1782ddf5
Sergeypanikov <svp1967@gmail.com>
2024-11-27 17:58:25 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# module_1_3
+name = "Sergey"
+print("Name:" + name)
+age = 56
+print("Age:" + "56")
+new_age = 57
+print("New_age:" + "57")
+name = "is_student"
+print("Is_student:")
+print(name == "is_student")
